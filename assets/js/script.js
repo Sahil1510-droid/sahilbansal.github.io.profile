053ce471671c38829f6e68e1911ecf867fef9bb0
Sahil Bansal <66123690+Sahil1510-droid@users.noreply.github.com>
2024-05-16 13:47:40 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,18 +59,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
   var span = document.getElementsByClassName("close")[0];
 
   // Show the modal when the page loads
-  modal.style.display = "block";
+  modal.style.display = "flex"; // Set display to flex instead of block
 
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    modal.style.display = "none";
+  span.onclick = function() {
+      modal.style.display = "none";
   }
 
   // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
   }
 });
 
