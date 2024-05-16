0bbd3745ea4fd6dc8eeeb38e73691584919e00fb
Sahil Bansal <66123690+Sahil1510-droid@users.noreply.github.com>
2024-05-16 12:30:18 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,3 +46,30 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+
+/**
+ * Modal
+ */
+ document.addEventListener('DOMContentLoaded', (event) => {
+      // Get the modal
+      var modal = document.getElementById("myModal");
+
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+
+      // Show the modal when the page loads
+      modal.style.display = "block";
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function () {
+        modal.style.display = "none";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    });
