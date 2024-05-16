1fb8a04a9118a79e91ad14b5af6988894cb0cde9
Sahil Bansal <66123690+Sahil1510-droid@users.noreply.github.com>
2024-05-16 14:59:47 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,98 +1,56 @@
 'use strict';
 
-
-
-/**
- * navbar toggle
- */
-
 const header = document.querySelector("[data-header]");
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 
 navToggleBtn.addEventListener("click", function () {
   header.classList.toggle("nav-active");
-  this.classList.toggle("active");
+  this.classList.toggle("active")
 });
 
-/**
- * toggle the navbar when click any navbar link
- */
-
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
-
 for (let i = 0; i < navbarLinks.length; i++) {
   navbarLinks[i].addEventListener("click", function () {
     header.classList.toggle("nav-active");
-    navToggleBtn.classList.toggle("active");
-  });
+    navToggleBtn.classList.toggle("active")
+  })
 }
 
 
-
-
-
-/**
- * back to top & header
- */
-
 const backTopBtn = document.querySelector("[data-back-to-top]");
-
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 100) {
     header.classList.add("active");
-    backTopBtn.classList.add("active");
+    backTopBtn.classList.add("active")
   } else {
     header.classList.remove("active");
-    backTopBtn.classList.remove("active");
+    backTopBtn.classList.remove("active")
   }
 });
 
-
-/**
- * Modal
- */
 document.addEventListener('DOMContentLoaded', (event) => {
-  // Get the modal
   var modal = document.getElementById("myModal");
-
-  // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
-
-  // Show the modal when the page loads
-  modal.style.display = "flex"; // Set display to flex instead of block
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-      modal.style.display = "none";
+  modal.style.display = "flex";
+  span.onclick = function () {
+    modal.style.display = "none"
   }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none"
+    }
   }
 });
 
-
-/**
- * Preloader
- */
 var loader = document.getElementById("preloader");
-
 window.addEventListener("load", function () {
-  loader.style.display = "none";
+  loader.style.display = "none"
 })
 
-
-/**
- * Smooth Scroll
- */
 function smoothScroll(element) {
   var top = element.offsetTop;
-  window.scrollTo(0, top);
+  window.scrollTo(0, top)
 }
-
 document.querySelectorAll("nav").forEach(function (element) {
-  element.addEventListener("click", smoothScroll);
-});
+  element.addEventListener("click", smoothScroll)
+})
